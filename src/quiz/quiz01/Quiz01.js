1b97b961e6c9cd9d52386c39513f47b253490b7c
안송이 <ansong-i@ansong-iui-iMac.local>
2025-12-04 10:06:09 +0900

    리액트 퀴즈1

---
+++ src/quiz/quiz01/Quiz01.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function Quiz01() {
+    let [count, setCount] = useState(0);
+
+
+    //let num = 0;
+    return (
+        <div>
+            <button onClick={() => {
+                setCount(count + 2)
+            }}>짝수출력</button>
+            <span style={{ color: 'red' }}>{count}</span>
+
+        </div>
+    )
+}
+
+export default Quiz01

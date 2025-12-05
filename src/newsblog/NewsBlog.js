13d87b12f54fa1bd33bef93bf10b2e93f34446ce
SONGI <ahnsongi6769@gmail.com>
2025-12-05 14:08:52 +0900

    모달창에 각종 props 전달 사용 방식

--- src/newsblog/NewsBlog.js
+++ src/newsblog/NewsBlog.js
@@ -1,20 +1,26 @@
 import './NewsBlog.css';
-import { useState } from 'react';
+import { use, useState } from 'react';
 import Modal from './Modal';
 
-
 function NewsBlog() {
-    // 하드코딩 (텍스트를 직접입력) vs 변수 vs state변수
-    let title = "React Study ";
+
+    //하드코딩(텍스트를 직접) vs  변수  vs state변수
+    let title = "React Study";
 
     let [news1, setNews1] = useState('오늘의 뉴스');
     let [news2, setNews2] = useState('어제의 뉴스');
     let [news3, setNews3] = useState('내일의 뉴스');
 
     let [news, setNews] = useState(['오늘의 뉴스', '어제의 뉴스', '내일의 뉴스']);
+    let [likeCountArr, setLikeCountArr] = useState([0, 0, 0]);
+
     let [likeCount, setLikeCount] = useState(0);
 
+
     let [modalFlag, setModalFlag] = useState(false);
+
+    let [selectedTitle, setSelectedTitle] = useState('');
+    let [selectedLikeCount, setSelectedLikeCount] = useState(0);
 
     return (
         <div>
@@ -23,53 +29,60 @@ function NewsBlog() {
                 <div style={{ color: "orange", fontSize: "20px" }}>{title}</div>
             </div>
 
-            <div className='post-list'>
-                <h4 onClick={()=>{
-                    
-                    setModalFlag(!modalFlag); // true -> false , !false -> true
-                    
-                    //setModalFlag( modalFlag ? false : true );
 
-                    // if(modalFlag==true){
-                    //     setModalFlag(false);
-                    // }else{
-                    //     setModalFlag(true);
-                    // }
-                }}>{news[0]}
-                    <span onClick={(event)=>{
-                        event.stopPropagation(); // 상위요소로 이벤트 전파 차단
-                    setLikeCount(likeCount + 1); 
-                }}>❤️</span>{likeCount}
-                </h4>
-                <p>내용</p>
-            </div>
-            <div className='post-list'>
-                <h4>{news[1]}</h4>
-                <p>내용</p>
-            </div>
-            <div className='post-list'>
-                <h4>{news[2]}</h4>
-                <p>내용</p>
-            </div>
+            {
+                news.map((item, index) => {
+                    return (
+                        <div className='post-list'>
+                            <h4 onClick={() => {
+                                setModalFlag(!modalFlag); //모달창 표시 On/Off 여부
+                                //어떤 뉴스를 눌렀는지 그 뉴스 제목 -> 저장 ---> Modal props 전달
+                                setSelectedTitle(item);
+                                setSelectedLikeCount(likeCountArr[index]);
+                            }}>{news[index]}
+                                <span onClick={(event) => {
+                                    event.stopPropagation();
+                                    let temp = [...likeCountArr];
+                                    temp[index] += 1;
+                                    setLikeCountArr(temp);
+                                }}> ❤</span> {likeCountArr[index]}
+                            </h4>
+                            <p>내용</p>
+                        </div>
+                    )
+                })
+            }
 
             {/* <div className='post-list'>
-                <h4>{news1}</h4>
+                <h4>오늘의 뉴스</h4>
                 <p>내용</p>
             </div>
             <div className='post-list'>
-                <h4>{news2}</h4> 
+                <h4>{news2}</h4>
                 <p>내용</p>
             </div>
             <div className='post-list'>
                 <h4>{news3}</h4>
                 <p>내용</p>
             </div> */}
+
             {
-                // modalFlag == true ? <modal/> : null
-                modalFlag && <Modal />  
+                /* modalFlag == true ? <Modal/> : null */
             }
 
-        </div >
+            <button onClick={()=>{
+                //'Today News'
+                let temp = [...news];
+                temp[0] = 'Today News';
+                setNews(temp);
+            }}>첫글 제목 변경</button>
+
+            {
+                modalFlag && <Modal news={news} setNews={setNews} bgColor={'lightblue'}
+                            title={selectedTitle} likeCount={selectedLikeCount} />
+            }
+
+        </div>
     )
 }
 

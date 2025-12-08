8e526a8c28895cc63cf39926021728528bb1cae9
SONGI <ahnsongi6769@gmail.com>
2025-12-08 14:01:56 +0900

    FoodCard컴포넌트화,foodsData 이미지경로 추가 및 public 경로 이미지 접근

--- src/foodmarket/FoodMarket.js
+++ src/foodmarket/FoodMarket.js
@@ -6,12 +6,18 @@ import './FoodMarket.css';
 import banner_bg from './img/banner_bg.jpg';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import {Row,Col} from 'react-bootstrap';
-import food1 from'./img/food1.jpg';
-import food2 from'./img/food2.jpg';
-import food3 from'./img/food3.jpg';
+import { Row, Col } from 'react-bootstrap';
+import food1 from './img/food1.jpg';
+import food2 from './img/food2.jpg';
+import food3 from './img/food3.jpg';
+//기준 data import
+import foodsData from './data/foodsDate';
+import { useState } from 'react';
+import FoodCard from './components/FoodCard';
 
 function FoodMarket() {
+
+    let [foods, setfoods] = useState(foodsData);
 
     return (
         <div>
@@ -28,14 +34,71 @@ function FoodMarket() {
 
             {/* 
                 javascript 이미지 사용 
+                1. css 에서는 이미지 경로 그대로 사용가능
+                2. js 사용할때는 라애 참고
                 react 컴포넌트 js 에서 이미지 사용 시 import 후 사용해야한다. 
                 <img src = "https://...외부 url/"/>
                 <img src = {import image}/>
+                3. public 폴더에 이미지 저장 후 사용 할 때
+                <img src ={'/images/food1.jpg}
             */}
             <div className='main-bg' style={{ backgroundImage: 'url(' + banner_bg + ')' }}></div>
             {/* <div className='main-bg'></div> */}
 
             <Container>
+                <Row>
+                    {
+                        foods.map((food, index) => {
+                            return (<Col md={4} sm={2}>
+                                <FoodCard food={food} foods={foods} index={index} />
+                            </Col>)
+                        })
+                    }
+
+                </Row>
+            </Container>
+
+
+
+            {/* <Container>
+                <Row>
+                    <Col md={4} sm={2}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={food1} />
+                            <Card.Body>
+                                <Card.Title>{foods[0].title}</Card.Title>
+                                <Card.Text>{foods[0].content} </Card.Text>
+                                <Card.Text>{foods[0].price} </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4} sm={2}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={food2} />
+                            <Card.Body>
+                                <Card.Title>{foods[1].title}</Card.Title>
+                                <Card.Text>{foods[1].content} </Card.Text>
+                                <Card.Text>{foods[1].price} </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4} sm={2}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={food3} />
+                            <Card.Body>
+                                <Card.Title>{foods[2].title}</Card.Title>
+                                <Card.Text>{foods[2].content} </Card.Text>
+                                <Card.Text>{foods[2].price} </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container> */}
+
+            {/* <Container>
                 <Row>
                     <Col md={4} sm={2}>
                         <Card style={{ width: '18rem' }}>
@@ -77,7 +140,7 @@ function FoodMarket() {
                         </Card>
                     </Col>
                 </Row>
-            </Container>
+            </Container> */}
         </div>
     )
 
